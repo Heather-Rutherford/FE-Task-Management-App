b6d8ab748691371c6cc274356d68da56009f7565
Heather Rutherford <rutherfordhd13@gmail.com>
2026-02-18 20:38:46 -0600

    Corrected some errors found from the last round of testing.

--- src/authentication/ProfilePage.tsx
+++ src/authentication/ProfilePage.tsx
@@ -1,6 +1,7 @@
 // ProfilePage.tsx
 import { useAuth0 } from "@auth0/auth0-react";
 import { type Auth0User } from "../models/Auth0.model";
+import PageLayout from "../components/PageLayout";
 
 const ProfilePage: React.FC = () => {
   const { user } = useAuth0();
@@ -9,8 +10,16 @@ const ProfilePage: React.FC = () => {
   if (!typedUser) return <div>No user data</div>;
 
   return (
-    <div>
-      <img src={typedUser.picture} alt="Profile" />
+    <PageLayout>
+      {typedUser.picture && (
+        <img
+          src={typedUser.picture}
+          alt="Profile"
+          onError={(e) => {
+            (e.target as HTMLImageElement).style.display = "none";
+          }}
+        />
+      )}
       <h2>User Profile for {typedUser.name}</h2>
       <p>User ID: {typedUser.sub}</p>
       <p>First Name: {typedUser.given_name}</p>
@@ -20,7 +29,7 @@ const ProfilePage: React.FC = () => {
       <p>Email: {typedUser.email}</p>
       <p>Email Verified: {typedUser.email_verified ? "Yes" : "No"}</p>
       <p>Last Updated: {typedUser.updated_at}</p>
-    </div>
+    </PageLayout>
   );
 };
 export default ProfilePage;
--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,42 +1,27 @@
 //NavBar.tsx
 import React from "react";
-import { Col, Nav, Navbar, Row } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
-
-import NavBarButtons from "./NavBarButtons";
+import LoginButton from "../authentication/LoginButton";
+import RegisterButton from "../authentication/RegisterButton";
+import LogoutButton from "../authentication/LogoutButton";
 
 const NavBar: React.FC = () => {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <Navbar>
-      <Nav>
-        <Col>
-          <Row>
-            <Nav.Link href="/">Home</Nav.Link>
-          </Row>
-          {isAuthenticated && (
-            <>
-              <Row>
-                <Nav.Link href="/DisplayTasks"> Display Tasks</Nav.Link>
-              </Row>
-              <Row>
-                <Nav.Link href="/add-task"> Create Task</Nav.Link>
-              </Row>
-              <Row>
-                <Nav.Link href="/ListTasks"> List Tasks </Nav.Link>
-              </Row>
-              <Row>
-                <Nav.Link href="/TaskReport">Task Report</Nav.Link>
-              </Row>
-              <Row>
-                <NavBarButtons />
-              </Row>
-            </>
-          )}
-        </Col>
-      </Nav>
-    </Navbar>
+    <div>
+      {!isAuthenticated && (
+        <div className="button-group">
+          <RegisterButton />
+          <LoginButton />
+        </div>
+      )}
+      {isAuthenticated && (
+        <>
+          <LogoutButton />
+        </>
+      )}
+    </div>
   );
 };
 
